c5020fe46e893ef206278c9a4cf112000750f9b5
Quentin Gautreau <qgautreau@gmail.com>
2017-03-06 17:16:18 +0100

    HTML fini, jquery en cours

---
+++ static/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    
+
+    $(".field").focusout(function(){
+        if($(this).val().length < 5){
+            $(this).parent().addClass("has-error");
+            $(this).tooltip("show");
+        }
+        else{
+            $(this).parent().removeClass("has-error");
+            $(this).parent().addClass("has-success");
+        }
+    });
+    $("#confirm").focusout(function(){
+        if($(this).val() != $("#password").val()){
+            $("#password").parent().removeClass("has-success");
+            $("#password").parent().addClass("has-error");
+            $("#confirm").parent().addClass("has-error");
+            $(this).popover("show");
+            $(this).tooltip("hide");
+        }
+    });
+    $("#signin").click(function(){
+        $("input")
+    });
+});
