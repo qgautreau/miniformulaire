9523b9d767bc99177f3ac5da192530312e0833f4
Quentin Gautreau <qgautreau@gmail.com>
2017-03-07 10:22:05 +0100

    Formulaire fonctionnel, test responsive en cours

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,31 @@
+// $(document).ready(function(){
+//     $(".field").focusout(function(){
+//         if($(this).val().length < 5){
+//             $(this).parent().addClass("has-error");
+//             $(this).tooltip("show");
+//         }
+//         else{
+//             $(this).parent().removeClass("has-error");
+//             $(this).parent().addClass("has-success");
+//         }
+//     });
+//     $("#confirm").focusout(function(){
+//         if($(this).val() != $("#password").val()){
+//             $("#password").parent().removeClass("has-success");
+//             $("#password").parent().addClass("has-error");
+//             $("#confirm").parent().addClass("has-error");
+//             $(this).popover("show");
+//             $(this).tooltip("hide");
+//         }
+//     });
+//     $("#signin").click(function(e){
+//         if($("input").val() = 0 || $("input").val() < 5){
+//             e.preventDefault();
+//
+//         }
+//
+//     });
+// });
 $(document).ready(function(){
-    
-
-    $(".field").focusout(function(){
-        if($(this).val().length < 5){
-            $(this).parent().addClass("has-error");
-            $(this).tooltip("show");
-        }
-        else{
-            $(this).parent().removeClass("has-error");
-            $(this).parent().addClass("has-success");
-        }
-    });
-    $("#confirm").focusout(function(){
-        if($(this).val() != $("#password").val()){
-            $("#password").parent().removeClass("has-success");
-            $("#password").parent().addClass("has-error");
-            $("#confirm").parent().addClass("has-error");
-            $(this).popover("show");
-            $(this).tooltip("hide");
-        }
-    });
-    $("#signin").click(function(){
-        $("input")
-    });
+    $("#inputPassword").pwstrength();
 });
